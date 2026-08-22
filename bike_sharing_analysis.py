d607705e4a111682aba958787010fe08bc7eafea
melonsoda <gpragya207@gmail.com>
2026-08-22 12:29:33 +0530

    loading and checking the csv file

--- bike_sharing_analysis.py
+++ bike_sharing_analysis.py
@@ -1,6 +1,18 @@
-# %%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# %%
+
+# Load the hourly data and check the data
+hour_df= pd.read_csv('hour.csv')
+print(hour_df.shape)
+print(hour_df.columns)
+hour_df.head()
+
+# check for missing values
+print(hour_df.isnull().sum())
+
+# check data types
+print(hour_df.dtypes)
+
 
