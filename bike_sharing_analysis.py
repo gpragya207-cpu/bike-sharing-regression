b913b246bd771677732ab658471280b1ffafbf83
melonsoda <gpragya207@gmail.com>
2026-08-22 21:13:30 +0530

    respective plots from py file

--- bike_sharing_analysis.py
+++ bike_sharing_analysis.py
@@ -2,16 +2,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+# %%
 # Load the hourly data and check the data
 hour_df= pd.read_csv('hour.csv')
 print(hour_df.shape)
 print(hour_df.columns)
 hour_df.head()
-
+# %%
 # check for missing values
 print(hour_df.isnull().sum())
-
+# %%
 # check data types
 print(hour_df.dtypes)
 
@@ -21,18 +21,21 @@ print(hour_df.dtypes)
 hour_df['cnt'].plot(kind= 'hist', bins= 40, title= 'Distribution of hourly rentals', color= 'blue')
 plt.xlabel('Rentals per hour')
 plt.ylabel('Frequency')
+plt.savefig('plots/cnt_distribution.png')
 plt.show() 
 
 # average rentals per hour of the day (line graph since we are looking avg rentals over time)
 hour_df.groupby('hr')['cnt'].mean().plot(kind= 'line', title= 'Average rentals by hour of the day', color= 'orange')
 plt.xlabel('Hour')
 plt.ylabel('Avg Rentals')
+plt.savefig('plots/avg_rental_per_hour.png')
 plt.show()
 # rentals go up during the day and peak around 8 am and 5 pm, which is expected as these are the peak commuting hours.
 
 # temperature vs rentals
 hour_df.plot(kind= 'scatter', x= 'temp', y= 'cnt', alpha=0.1 , title= 'Temperature vs Rentals', color= 'green')
 plt.xlabel('Temperature (normalized)')
+plt.savefig('plots/temp_vs_rentals.png')
 plt.show()
 # scatter plot shows a positiive correlation between temperature and rentals, which is expected as people are more likely to rent bikes in warmer weather.
 
@@ -40,6 +43,7 @@ plt.show()
 hour_df.groupby('weathersit')['cnt'].mean().plot(kind='bar', title='Avg rentals by weather condition')
 plt.xlabel('Weather situation (1=clear, 4=heavy rain/snow)')
 plt.ylabel('Avg rentals')
+plt.savefig('plots/weather_vs_rentals.png')
 plt.show()
 # similar to temperature, rentals are higher in clear weather and lower in bad weather conditions.
 
@@ -47,6 +51,7 @@ plt.show()
 hour_df.plot(kind='scatter', x='hum', y='cnt', alpha=0.1, title='Humidity vs Rentals')
 plt.xlabel('Humidity (normalized)')
 plt.ylabel('Rentals')
+plt.savefig('plots/hum_vs_rentals.png')
 plt.show()
 # not much correlation between humidity and rentals, but there is a slight negative correlation, which is expected as people are less likely to rent bikes in high humidity.
 
@@ -54,6 +59,7 @@ plt.show()
 hour_df.groupby('season')['cnt'].mean().plot(kind='bar', title='Avg rentals by season')
 plt.xlabel('Season (1=winter, 2=spring, 3=summer, 4=fall)')
 plt.ylabel('Avg rentals')
+plt.savefig('plots/rental_by_season.png')
 plt.show()
 # rentals are highest in summer and lowest in winter, which is expected as people are more likely to rent bikes in warmer weather.
 
@@ -95,6 +101,7 @@ plt.ylabel('Rentals')
 xs= np.linspace(hour_df.get('temp').min(), hour_df.get('temp').max(), 100)
 plt.plot(xs, temp_slope* xs + temp_intercept, color='red', label= 'Regression Line')
 plt.legend()
+plt.savefig('plots/temp_regression_line.png')
 plt.show()
 # %%
 # plotting the residuals to check for patterns with RMSE
@@ -106,6 +113,7 @@ plt.title('Residuals of Temperature vs Rentals Regression')
 plt.xlabel('Temperature (normalized)')
 plt.ylabel('Residuals')
 plt.legend()
+plt.savefig('plots/temp_residual_plot.png')
 plt.show()
 # residual fan out at higher temperatures and rather clustered at low temperatures (heteroscedasticity)
 # %%
@@ -150,8 +158,6 @@ print(f'Intercept: {model.intercept_}')
 # the coefficients indicate the change in rentals for a unit change in the predictor variable, holding all other variables constant.
 # temp has the highest positive coeff and hum has the highest negative coeff
 
-# %%
-
 # previous code showed which variables matter and in what direction; now checking how well 
 # the model predicts on unseen data (train/test split)
 
@@ -179,4 +185,42 @@ print(f"R²: {r2}")
 # the multiple regression model explains approximately 54% of the variance in hourly bike rentals (R² = 0.54)
 # rest of the variation remains unexplained likely due to other factors not included in the model
 
+# testiing the model on a single example to see how it performs
 # %%
+print(X.columns.tolist())
+# %%
+# creating a dictionary with key value pairs in the same order as the columns
+conditions= {
+    'temp': 0.15, #since its normalised
+    'hum': 0.85,
+    'weathersit_2': 0,
+    'weathersit_3': 1,
+    'weathersit_4': 0,
+    'season_2': 0,
+    'season_3': 0,
+    'season_4': 0, #season 1 (winter) is the base case
+    'hr_cat_Evening Rush': 1,
+    'hr_cat_Midday': 0,
+    'hr_cat_Morning Rush': 0,
+    'hr_cat_Night': 0 # evening is the base case
+}
+cond_df= pd.DataFrame([conditions], index=[0])
+print(cond_df)
+
+# %%
+# quick check to see if the column order is correct
+scenario = cond_df[X.columns.tolist()]
+print(len(scenario.columns) ==len(X.columns)) # evaluated to True
+# %%
+
+# predict the rentals for the given scenario
+predicted_rentals_model = eval_model.predict(scenario)
+print(f"Predicted rentals for the given scenario: {predicted_rentals_model[0]}")
+# %%
+check = hour_df[(hour_df['season'] == 1) & (hour_df['weathersit'] == 3) & (hour_df['hr'].between(16,19))]
+print(check.shape[0])
+print(check['cnt'].mean(), len(check)) # came out to be around 118
+# this gap likely reflects the model's linear, additive structure 
+# it assumes each factor's effect (temperature, humidity, weather, time-of-day) contributes independently, 
+# but in reality, rush-hour demand may be less weather-sensitive
+
